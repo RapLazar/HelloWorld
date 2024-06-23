4d88e67af1e8832ddee2df5155c211ba1cd6483e
GabJibs <enddorf@gmail.com>
2024-06-23 11:19:13 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,3 +1,3 @@
 
 
-class Hello { public static void main(String[] args) { System.out.println("Hello ka dyan, Gab"); } }
+class Hello { public static void main(String[] args) { System.out.println("Hello ka dyan, Gab jalgfahoijhroayh"); } }
